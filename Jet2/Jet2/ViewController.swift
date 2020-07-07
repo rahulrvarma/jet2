a9bd55cb5940850f66db107728d47a1f66976f5f
Rahul Varma <rahulrvarma22@gmail.com>
2020-07-07 11:06:05 +0530

    New project initiated for Jet2.

---
+++ Jet2/Jet2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jet2
+//
+//  Created by Rahul Varma on 07/07/20.
+//  Copyright © 2020 Rahul Varma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
