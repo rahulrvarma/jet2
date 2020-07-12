f7a11b708eca60d31c51f42db039cc927008b92c
Rahul Varma <rahulrvarma22@gmail.com>
2020-07-13 00:24:53 +0530

    Navigate to user profile on article cell click

--- Jet2/Jet2/ViewControllers/ArticleViewController.swift
+++ Jet2/Jet2/ViewControllers/ArticleViewController.swift
@@ -94,10 +94,10 @@ extension ArticleViewController : UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         
-        self.navigationController?.pushViewController(UsersViewController(), animated: true)
+//        self.navigationController?.pushViewController(UsersViewController(), animated: true)
 
-//        let profileVC = ProfileViewController(user: self.arrArticles[indexPath.row].user.first!)
-//        self.navigationController?.pushViewController(profileVC, animated: true)
+        let profileVC = ProfileViewController(user: self.arrArticles[indexPath.row].user.first!)
+        self.navigationController?.pushViewController(profileVC, animated: true)
 
     }
     
